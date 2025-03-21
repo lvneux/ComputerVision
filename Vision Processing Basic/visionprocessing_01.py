d2c3ea1173ec868a9ae60fa88348ef0295986b3d
lvneux <104901260+lvneux@users.noreply.github.com>
2025-03-21 10:16:03 +0900

    Update visionprocessing_01.py

--- Vision Processing Basic/visionprocessing_01.py
+++ Vision Processing Basic/visionprocessing_01.py
@@ -7,9 +7,11 @@ gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
 t, bin_img = cv.threshold(gray[:,:], 127, 255, cv.THRESH_BINARY)
 
-h=cv.calcHist([bin_img],[0],None,[256],[0,256])
-h2=h=cv.calcHist([gray],[0],None,[256],[0,256])
+h=cv.calcHist([gray],[0],None,[256],[0,256])
+h2=cv.calcHist([bin_img],[0],None,[256],[0,256])
+
 plt.imshow(bin_img,cmap='gray'),plt.xticks([]),plt.yticks([]),plt.show()
 plt.plot(h,color='r',linewidth=1)
+plt.show()
 plt.plot(h2,color='r',linewidth=1)
 plt.show()
