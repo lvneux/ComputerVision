8852879f85a1191b13b520a959bbd18980f62496
lvneux <104901260+lvneux@users.noreply.github.com>
2025-04-02 00:06:53 +0900

    Update local_feature_03.py

--- Local Feature/local_feature_03.py
+++ Local Feature/local_feature_03.py
@@ -1,9 +1,9 @@
 import cv2 as cv
 import numpy as np
 
-img1 = cv.imread('img1.jpg')
+img1 = cv.imread('./imgs/img1.jpg')
 gray1 = cv.cvtColor(img1, cv.COLOR_BGR2GRAY)
-img2 = cv.imread('img2.jpg')
+img2 = cv.imread('./imgs/img2.jpg')
 gray2 = cv.cvtColor(img2, cv.COLOR_BGR2GRAY)
 
 sift = cv.SIFT_create()
@@ -14,26 +14,29 @@ bf_matcher = cv.BFMatcher_create(cv.NORM_L2)
 matches = bf_matcher.knnMatch(des1, des2, 2)
 
 T = 0.7
-good_matches = [m for m, n in matches if m.distance < T * n.distance]
+good_match = []
+for nearest1, nearest2 in matches:
+    if (nearest1.distance/nearest2.distance)<T:
+        good_match.append(nearest1)
 
-if len(good_matches) > 4:  
+if len(good_match) > 4:  
 
-    points1 = np.float32([kp1[m.queryIdx].pt for m in good_matches])
-    points2 = np.float32([kp2[m.trainIdx].pt for m in good_matches])
+    points1 = np.float32([kp1[m.queryIdx].pt for m in good_match])
+    points2 = np.float32([kp2[m.trainIdx].pt for m in good_match])
 
     H, mask = cv.findHomography(points1, points2, cv.RANSAC)
 
     h, w = img2.shape[0], img2.shape[1]
     img1_warped = cv.warpPerspective(img1, H, (w, h))
 
-    side_by_side = np.hstack((img1_warped, img1))
+    imgs = np.hstack((img1_warped, img1))
     
     comparison = cv.addWeighted(img1_warped, 0.5, img2, 0.5, 0)
 
-    cv.imshow('Warped Image', side_by_side)
+    cv.imshow('Warped Image', imgs)
     cv.imshow('Comparison', comparison)
 
-    img_match = cv.drawMatches(img1, kp1, img2, kp2, good_matches, None, 
+    img_match = cv.drawMatches(img1, kp1, img2, kp2, good_match, None, 
                                flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
     cv.imshow('Feature Matches', img_match)
 
